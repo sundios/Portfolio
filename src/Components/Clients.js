0657d42ac91818d3a7f3370151e4d8184ad00d1c
sundios <sundios@gmail.com>
2019-08-15 16:43:31 +0200

    first commit

---
+++ src/Components/Clients.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class Clients extends Component {
+  render() {
+
+    if(this.props.data){
+      var brands = this.props.data.brands.map(function(brands){
+        var projectImage = 'images/portfolio/'+brands.image;
+        return <div key={brands.title} className="columns portfolio-item">
+           <div className="item-wrap">
+            <h1 title={brands.title}>
+               <img alt={brands.title} src={projectImage} />
+               <div className="overlay">
+                  <div className="portfolio-item-meta">
+                 <h5>{brands.title}</h5>
+                     <p>{brands.category}</p>
+                  </div>
+                </div>
+              <div className="link-icon"><i className="fa fa-link"></i></div>
+            </h1>
+          </div>
+        </div>
+      })
+    }
+
+    return (
+      <section id="clients">
+
+      <div className="row">
+
+         <div className="twelve columns collapsed">
+
+            <h1>Clients I have work with.</h1>
+
+            <div id="clients-wrapper" className="bgrid-quarters">
+                {brands}
+            </div>
+          </div>
+      </div>
+   </section>
+    );
+  }
+}
+
+export default Clients;
