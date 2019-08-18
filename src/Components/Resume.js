373196ee0de4eac9227d8ce9577323585328ef01
sundios <sundios@gmail.com>
2019-08-18 15:43:09 +0200

    finishing skills

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import Seo from './seo'
-import Code from './code'
+
 
 class Resume extends Component {
   render() {
@@ -17,6 +16,49 @@ class Resume extends Component {
             <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
             <p>{work.description}</p>
         </div>
+      })
+      var seo = this.props.data.seo.map(function(seo){
+        var projectImage = 'images/tools-img/'+seo.image;
+        return <div key={seo.title}>
+              <a href={seo.url} title={seo.title} target="_blank">
+                <div className="box">
+                  <img src={projectImage} alt={seo.name + " logo"} />
+                    <h6> {seo.name}</h6>
+                </div>
+               </a> 
+              </div> 
+      })
+      var code = this.props.data.code.map(function(code){
+        var projectImage = 'images/tools-img/'+code.image;
+        return <div key={code.title}>
+              <a href={code.url} title={code.title} target="_blank">
+                <div className="box">
+                  <img src={projectImage} alt={code.name + " logo"} />
+                    <h6> {code.name}</h6>
+                </div>
+               </a> 
+              </div> 
+      })
+      var tools = this.props.data.tools.map(function(tools){
+        var projectImage = 'images/tools-img/'+tools.image;
+        return <div key={tools.title}>
+              <a href={tools.url} title={tools.title} target="_blank">
+                <div className="box">
+                  <img src={projectImage} alt={tools.name + " logo"} />
+                    <h6> {tools.name}</h6>
+                </div>
+               </a> 
+              </div> 
+      })
+
+      var languages = this.props.data.languages.map(function(languages){
+        var projectImage = 'images/languages/'+languages.image;
+        return <div key={languages.title}>
+                <div className="box">
+                  <img src={projectImage} alt={languages.name} />
+                    <h6> {languages.name}</h6>
+                </div>
+              </div> 
       })
     }
 
@@ -48,13 +90,10 @@ class Resume extends Component {
           {work}
         </div>
     </div>
-
-
-
       <div className="row skill">
 
          <div className="three columns header-col">
-            <h1><span>Profesional Skills</span></h1>
+            <h1><span>Pro Skills</span></h1>
          </div>
 
          <div className="nine columns main-col">
@@ -62,8 +101,26 @@ class Resume extends Component {
             <p>{skillmessage}
             </p>
 			</div>
-      <Seo/>
-       <Code/>
+      <div className="skill_column">
+      <h1> SEO </h1>
+      {seo}
+      </div>
+
+      <div className="skill_column">
+      <h1> Code & Tech </h1>
+      {code}
+      </div>
+
+      <div className="skill_column">
+      <h1> Other tools </h1>
+      {tools}
+      </div>
+
+      <div className="skill_column">
+      <h1> Languages </h1>
+      {languages}
+      </div>
+
       </div>
    </section>
     );
